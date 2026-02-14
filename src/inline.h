30a666ec82ad89d42b188e58fd709a1feba87af9
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-02-14 17:22:34 -0500

    Update inline.h

--- src/inline.h
+++ src/inline.h
@@ -62,7 +62,8 @@ typedef struct {
 } inline_colorspan_t;
 
 /** @brief Syntax coloring callback function, called repeatedly by
- *         the editor to obtain the next colored span.
+ *         the editor to obtain the next colored span. The span is assumed 
+ *         to begin as offset.
  *  @param[in]  utf8    The full buffer encoded as UTF-8 to analyze.
  *  @param[in]  ref     User-supplied reference pointer.
  *  @param[in]  offset  Byte offset at which to begin scanning.
@@ -242,3 +243,4 @@ void inline_emitcolor(int color);
 void inline_displaywithsyntaxcoloring(inline_editor *edit, const char *string);
 
 #endif /* INLINE_H */
+
